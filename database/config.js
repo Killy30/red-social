4bfaa8e97d0f34bb178a51b678c593027aec9896
Killy30 <killy17za@gmail.com>
2022-06-11 23:30:07 -0400

    add data base code

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/BaseDeDato', {
+mongoose.connect('mongodb+srv://killy:postLine-db@cluster0.4d4vw.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true 
 })
   .then(db => console.log('database conectado '))
