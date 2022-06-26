6a0891f82087f3fe965358c27f76c6d0e6690db1
Killy30 <killy17za@gmail.com>
2022-06-26 18:15:07 -0400

    fix mongodb error

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb data base post line ==> mongodb+srv://killy:postLine-db@cluster0.4d4vw.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://killy:postLine-db@cluster0.4d4vw.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true 
 })
   .then(db => console.log('Connected to Database'))
